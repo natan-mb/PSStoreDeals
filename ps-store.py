765b8a3522621aaa00474b5ca8cdbe64fbbd6aae
natan-mb <78055688+natan-mb@users.noreply.github.com>
2021-09-25 14:00:26 -0400

    Update ps-store.py
    cleaned up code

--- ps-store.py
+++ ps-store.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import time
 import csv
-#from ProfObj import *
 
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -15,13 +14,15 @@ filename = 'GamesUnder20Sale.csv'
 f = open(filename, 'w')
 f.write('Name, Price, % Off\n')
 
-page = 1
+# enter number of pages on the ps store website for the specific deal 
+pages = 10
+
+#link to the first page of a ps store sale
 url_to_scrape = "https://store.playstation.com/en-us/category/30826e82-088f-4cc2-aaa4-81507aa31353/1"
-#url_to_scrape = 'https://store.playstation.com/en-us/category/fc08491d-94a7-4252-96dd-9d4f54b56041/'+ str(page) + '/'
-#url_to_scrape = "https://store.playstation.com/en-us/category/b81cc589-d207-44dd-81ad-a63e15e05790/" + str(page) + "/"
 driver.get(url_to_scrape)
 
-while page <= 13:
+i = 0
+while i <= pages:
 
     games = []
 
@@ -31,43 +32,41 @@ while page <= 13:
             EC.presence_of_element_located((By.CSS_SELECTOR, "#main > section > div > div > div > div.psw-l-w-3\/4 > div.psw-l-w-1\/1 > ul > li:nth-child(5) > div > a > div > section > span"))
         )
         games = driver.find_elements_by_css_selector("#main > section > div > div > div > div.psw-l-w-3\/4 > div.psw-l-w-1\/1 > ul > li:nth-child(4) > div > a > div > section")
-        #games = driver.find_elements_by_xpath('/html/body/div[3]/main/section/div/div/div/div[2]/div[2]/ul/li[4]/div/a/div/section')
     except:
+        print("Time limit exceeded: could not find element")
         driver.quit()
+        break;
 
-    j = 1
-    i = -1
+    
+    i = -1  # counter variable, doubles as a tracker to advance to the next game entry
+    j = 1   # adjusts xpath because price and percent_off have inconisistent xpath values between games
     isFinished = 1
 
+    # run while i < (number of results per page - 1)
     while i < 23:
 
+        #NOTE: I made the choice to select elements by xpath because class name didn't seem to work, and css selector returned inconsistent values
+        
         isFinished += 1
         i += 1
         try:
-
-            try:
+           
+            try:    # xpath sometimes selects "add-on" or other game tags instead of the actual game name
                 game_name = games[0].find_element_by_xpath('//*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[' + str(i+1) + ']/div/a/div/section/span[2]').text
-                                                          # //*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[       14       ]/div/a/div/section/span
-                                                          # //*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[       19       ]/div/a/div/section/span
             except NoSuchElementException:
-                game_name = games[0].find_element_by_xpath('//*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[' + str(i + 1) + ']/div/a/div/section/span').text
-                                                          # //*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[        1         ]/div/a/div/section/span
+                game_name = games[0].find_element_by_xpath('//*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[' + str(i+1) + ']/div/a/div/section/span').text
 
             price = games[0].find_element_by_xpath('//*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[' + str(i+1) + ']/div/a/div/section/div[' + str(j+1) + ']/div/span[1]').text
-                                                  # //*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[      14        ]/div/a/div/section/div[        3       ]/div/span[1]
-                                                  # //*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[      15        ]/div/a/div/section/div[        2       ]/div/span[1]
-                                                  # //*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[      19        ]/div/a/div/section/div[        2       ]/div/span[1]
-            percent_off = games[0].find_element_by_xpath('//*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[' + str(i+1) + ']/div/a/div/section/div[' + str(j) + ']/span').text
-                                                        # //*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[      14        ]/div/a/div/section/div[      2       ]/span
-                                                        # //*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[      19        ]/div/a/div/section/div[      1       ]/span
+
+            percent_off = games[0].find_element_by_xpath('//*[@id="main"]/section/div/div/div/div[2]/div[2]/ul/li[' + str(i+1) + ']/div/a/div/section/div[' + str(j) + ']/span').tex
 
 
-            if "," in game_name:
+            if "," in game_name:    #comma in name leads to inconsistencies when writing to csv
                 game_name = game_name.replace(",", "-")
 
+            #optional, removes the minus sign (e.g. -80% -> 80%)
             percent_off = percent_off.replace("-", "")
 
-            #print(game_name + ", " + price + ", " + percent_off + "\n")
             f.write(game_name + "," + price + "," + percent_off + "\n")
 
         except NoSuchElementException:
@@ -76,7 +75,7 @@ while page <= 13:
             elif j == 2:
                 j -= 1
 
-            if isFinished >= 40:
+            if isFinished > 40:    # protects against infinite loop, loop runs at most 40 times per page
                 break
 
             i -= 1
@@ -85,10 +84,11 @@ while page <= 13:
         except:
             print("an unexpected error has occurred")
             break
-
+    
+    #advance to next page
     driver.find_element_by_css_selector('#main > section > div > div > div > div.psw-l-w-3\/4 > div.psw-l-w-1\/1 > div > nav > button:nth-child(3) > span > span > svg').click()
-    #time.sleep(5)
-    page += 1
+    
+    i += 1
 
 f.close()
 driver.quit()
